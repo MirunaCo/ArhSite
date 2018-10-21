e0aba98d80d8e77fe7106f375869457e84afba24
CoseriuMiruna <mirunacoseriu@gmail.com>
2018-10-21 16:10:23 +0300

    Added language controller and support for menu in romanian and english. Still need to call it. Also commented some of the main content

---
+++ ArhSite/Controllers/LanguagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArhSite.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(String strLanguageAbbrevation)
+        {
+            if (strLanguageAbbrevation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(strLanguageAbbrevation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(strLanguageAbbrevation);
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = strLanguageAbbrevation;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
